c299109df6dc3d2c336b5d4c79d1ce17b703a43e
P-jpg-hue <priyanshu.saini@rdpolytech.ca>
2025-03-20 20:09:03 -0600

    Update

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
-//this imports express  
 const express = require('express');
+const router = require("./routes/pages");
 
-//use dotenv to expose our environment variables
-require('dotenv').config();
-
-//we create a new instance of express as the app object 
 const app = express();
-//PORT assignment
-const PORT = process.env.PORT;
+const PORT = 3000;
 
-//route handler that will define uri/url that does something
-app.get('/home', (req, res) =>{
-  res.send('Hello, World');
-});
-
-//router handler that uses post. post creates resources
-app.post('/user/:id', (req, res) =>{
-    const user_id = req.params.id;
-
-  res.send(`The user id is: ${user_id}`);
-});
+//implementing routes
+app.use(router);
 
 
-//start our server
 app.listen(PORT, () =>{
-    console.log(`Server started on port: ${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
